4672669c36461d9d6b31d5a0092c219feb8b5ff0
Saksham-G9 <guptasaksham495@gmail.com>
2026-03-04 14:59:34 +0530

    Refactor isAnagram functions to separate implementations for counter, sorting, and bucket methods

--- 03_valid_anagram.py
+++ 03_valid_anagram.py
@@ -1,8 +1,32 @@
 from collections import Counter
 
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        return Counter(s) == Counter(t)
+def isAnagram_counter_sol(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    return Counter(s) == Counter(t)
+
+
+def isAnagram_sort_sol(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    return sorted(s) == sorted(t)
+
+
+def isAnagram_bucket_sol(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    freq = [0] * 26
+
+    for i in range(len(s)):
+        freq[ord(s[i]) - ord("a")] += 1
+        freq[ord(t[i]) - ord("a")] -= 1
+
+    return all(x == 0 for x in freq)
+
+
+s = "anagram"
+t = "nagaram"
+
+isAnagram_bucket_sol(s, t)
